194cab52056d10bcb7a35feb973768624d3aeb38
titol2003 <titolospina@gmail.com>
2022-10-07 10:44:11 -0500

    version1

---
+++ src/main/java/com/usa/grupo13/proyectomoto/entities/Nuevo.java
@@ -0,0 +1,63 @@
+package com.usa.grupo13.proyectomoto.entities;
+
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.List;
+
+
+@Entity
+@Table (name = "category")
+public class Nuevo implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String description;
+
+    @OneToMany(cascade = {CascadeType.PERSIST},mappedBy = "category")
+    @JsonIgnoreProperties({"motorbikes","category"})
+    private List<Moto> motorbikes;
+
+
+    public List<Moto> getMotorbikes() {
+        return motorbikes;
+    }
+
+    public void setMotorbikes(List<Moto> motorbikes) {
+        this.motorbikes = motorbikes;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
+
+
+
+}
